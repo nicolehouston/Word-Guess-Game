96ff12b3c71823ae19aa9d7df50f00dc616bcac7
Nicole Houston <nicolehouston@dhcp-v109-230.mobile.uci.edu>
2018-05-09 09:26:11 -0700

    Created an array to hold the words, some variable for remaining guesses and wins

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+// Create an array to store the words that users must guess.
+var words = ["taliyah", "yasuo", "sejuani"];
+
+// Create variable to keep track of remaining guesses and number of wins.
+var remainingGuesses = 12;
+var wins = 0;
+
+// Set the first word.
+document.getElementById("currentWord").innerHTML = words[0];
+
+
+// This function is run whenever a user presses a key.
+document.onkeyup = function (event) {
+
+// Determine which key was pressed.
+var userGuess = event.key;
+
+
+
+
+
+}
+
