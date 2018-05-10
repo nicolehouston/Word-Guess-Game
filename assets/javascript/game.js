d10b2837cb16b81a2a7118ab729c373ba444b4dd
Nicole Houston <nicolehouston@Nicoles-MacBook-Pro.local>
2018-05-09 21:54:02 -0700

    Wrote the startGame function to take word and write it into blanks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,15 +4,29 @@ var words = ["taliyah", "yasuo", "sejuani"];
 // Create variable to keep track of remaining guesses and number of wins.
 var remainingGuesses = 12;
 var wins = 0;
+var notInWord = [];
 
-// Set the first word.
-document.getElementById("currentWord").innerHTML = words[0];
+
+// Start the game
+function startGame(){
+    // Take the first word and create blanks for each letter in the word.
+    for (i = 0; i < words[0].length; i++) {
+        words[0] = words[0].replace(words[0].charAt(i), "_");
+    }
+    var blankWord = words[0].split();
+    // Write the blanks in the html and set remaining guesses to 12.
+    document.getElementById("currentWord").innerHTML = blankWord;
+    document.getElementById("guessesLeft").innerHTML = "Guesses remaining: " + remainingGuesses;
+}
+
+startGame();
+
 
 
 // This function is run whenever a user presses a key.
 document.onkeyup = function (event) {
 
-// Determine which key was pressed.
+// Store the user's guess of a letter in a variable.
 var userGuess = event.key;
 
 
