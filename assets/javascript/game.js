e3c301b9d242146a0789df5adc93e815f33d8512
Nicole Houston <nicolehouston@Nicoles-MacBook-Pro.local>
2018-05-09 23:23:15 -0700

    Wrote the javascript for what happens if a user guesses a letter that is not in the current word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,32 @@
 // Create an array to store the words that users must guess.
 var words = ["taliyah", "yasuo", "sejuani"];
+var currentWord = words[0];
 
 // Create variable to keep track of remaining guesses and number of wins.
 var remainingGuesses = 12;
 var wins = 0;
 var notInWord = [];
 
-
-// Start the game
-function startGame(){
-    // Take the first word and create blanks for each letter in the word.
-    for (i = 0; i < words[0].length; i++) {
-        words[0] = words[0].replace(words[0].charAt(i), "_");
+// Create function that takes a word and replaces each letter to be a blank.
+function displayBlanks(string) {
+    for (i = 0; i < string.length; i++) {
+        string = string.replace(string.charAt(i), "_");
     }
-    var blankWord = words[0].split();
-    // Write the blanks in the html and set remaining guesses to 12.
-    document.getElementById("currentWord").innerHTML = blankWord;
+        var result = string;
+        return result;
+}
+
+// Start the game function
+function startGame(string){
+    var wordAsBlanks = displayBlanks(string);
+    // Write the blanks in the html for the first word and set remaining guesses to 12.
+    document.getElementById("currentWord").innerHTML = wordAsBlanks;
     document.getElementById("guessesLeft").innerHTML = "Guesses remaining: " + remainingGuesses;
 }
 
-startGame();
+// Play Game
+// ========================================================
+startGame(currentWord);
 
 
 
@@ -27,11 +34,16 @@ startGame();
 document.onkeyup = function (event) {
 
 // Store the user's guess of a letter in a variable.
-var userGuess = event.key;
+var userGuess = event.key.toLowerCase();
 
-
-
-
+    if(userGuess !== currentWord.charAt(i)) {
+        remainingGuesses --;
+        document.getElementById("guessesLeft").innerHTML = "Guesses remaining: " + remainingGuesses;
+        notInWord.push(userGuess);
+        document.getElementById("lettersGuessed").innerHTML = "Incorrect Guesses: " + notInWord;
+     }
+        
+  
 
 }
 
