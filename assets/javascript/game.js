eeb74352ef934acb0251764567860843a1c8a7b7
Nicole Houston <nicolehouston@Nicoles-MacBook-Pro.local>
2018-05-14 19:55:12 -0700

    Restructured so that level restarts if remaining guesses = 0. Also added a losses variable to keep track of both wins and losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,21 +6,16 @@ var images = ["https://vignette.wikia.nocookie.net/disney/images/e/eb/Aladdin_Po
 var wordIndex = 0;
 var remainingGuesses = 8;
 var wins = 0;
+var losses = 0;
 var notInWord = [];
 var currentWord = '';
 var wordAsArray = [];
 var wordToGuess = makeArray(words[0]);
+var restartGame = false;
 
 // FUNCTIONS
 // =============================================================================
 // Create function that takes a word and replaces each letter to be a blank.
-function displayBlanks(string) {
-    for (i = 0; i < string.length; i++) {
-        string = string.replace(string.charAt(i), "_");
-    }
-        return string;
-}
-
 function makeArray(string) {
     string = string.split(" ");
     return string;
@@ -42,6 +37,7 @@ function startGame(string){
     currentWord = makeString(wordAsArray);
     displayCurrentWord(currentWord);
     document.getElementById("guessesLeft").innerHTML = "Guesses remaining: " + remainingGuesses;
+    restartGame = false;
     
 }
 
@@ -53,61 +49,82 @@ function changeWord(string) {
     wordToGuess = makeArray(string);
 }
 
+function restartRound(string) {
+    remainingGuesses = 8;
+    document.getElementById("guessesLeft").innerHTML = "Guesses remaining: " +remainingGuesses;
+    notInWord = [];
+    document.getElementById("lettersGuessed").innerHTML = "Letters already guessed: " + notInWord;
+    wordAsArray = makeArray(string);
+    wordAsArray.fill("_", 0, wordAsArray.length);
+    currentWord = makeString(wordAsArray);
+    displayCurrentWord(currentWord);
+    document.getElementById("numOfWins").innerHTML = "Wins: " + wins;
+    restartGame = false;
+
+}
+
+
 // Play Game
 // =============================================================================
 startGame(words[wordIndex]);
 
 
-
 // This function is run whenever a user presses a key.
 document.onkeyup = function (event) {
+    
+    if(restartGame !== true){
+        // Store the user's guess of a letter in a variable.
+        var userGuess = event.key.toLowerCase();
+        var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-    // Store the user's guess of a letter in a variable.
-    var userGuess = event.key.toLowerCase();
-    var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+        // Checks to see if userGuess equals a letter in the word to guess and then displays revealed letters.
+        for(i = 0; i < wordToGuess.length; i++){
+            if (userGuess === wordToGuess[i]) {
+                wordAsArray = makeArray(currentWord);
+                wordAsArray.splice(i, 1, userGuess);
+                currentWord = makeString(wordAsArray);
+                displayCurrentWord(currentWord);
+            }
+        } 
 
-    // Checks to see if userGuess equals a letter in the word to guess and then displays revealed letters.
-    for(i = 0; i < wordToGuess.length; i++){
-        if (userGuess === wordToGuess[i]) {
-            wordAsArray = makeArray(currentWord);
-            wordAsArray.splice(i, 1, userGuess);
-            currentWord = makeString(wordAsArray);
-            displayCurrentWord(currentWord);
+        // If a guessed letter is not in the word, then the letter gets added to the "letters guessed" list and remaining guesses decreases by one
+        if ((wordToGuess.indexOf(userGuess) === -1) && (notInWord.indexOf(userGuess) === -1) && (alphabet.indexOf(userGuess) !== -1)) {
+            remainingGuesses --;
+            document.getElementById("guessesLeft").innerHTML = "Guesses remaining: " +remainingGuesses;
+            notInWord.push(userGuess);
+            document.getElementById("lettersGuessed").innerHTML = "Letters already guessed: " + notInWord; 
+
+            if (remainingGuesses === 0) {
+                restartGame = true;
+            }
         }
-    } 
 
-    // If a guessed letter is not in the word, then the letter gets added to the "letters guessed" list and remaining guesses decreases by one
-    if ((wordToGuess.indexOf(userGuess) === -1) && (notInWord.indexOf(userGuess) === -1) && (alphabet.indexOf(userGuess) !== -1)) {
-        remainingGuesses --;
-        document.getElementById("guessesLeft").innerHTML = "Guesses remaining: " +remainingGuesses;
-        notInWord.push(userGuess);
-        document.getElementById("lettersGuessed").innerHTML = "Letters already guessed: " + notInWord;
-
-        if (remainingGuesses === 0) {
-            document.querySelector("body").innerHTML = "<p id='loseScreen'>Sorry, you are out of guesses! Better luck next time!</p>";
-            
-        }
-    }
-
-    // Checks to see if all the letters have been correctly guessed and then changes the word.
-    if ((currentWord === makeString(wordToGuess)) && wordIndex < words.length) {
-        document.getElementById("charImage").src = images[wordIndex];
-        document.getElementById("charName").innerHTML = words[wordIndex];
-        wins ++;
-        document.getElementById("numOfWins").innerHTML = "Wins: " + wins;
-        wordIndex ++;
-        remainingGuesses = 8;
-        document.getElementById("guessesLeft").innerHTML = "Guesses remaining: " +remainingGuesses;
-        notInWord = [];
-        document.getElementById("lettersGuessed").innerHTML = "Letters already guessed: " + notInWord;
-        changeWord(words[wordIndex]); 
+        // Checks to see if all the letters have been correctly guessed and then changes the word.
+        if ((currentWord === makeString(wordToGuess)) && wordIndex < words.length) {
+            document.getElementById("charImage").src = images[wordIndex];
+            document.getElementById("charName").innerHTML = words[wordIndex];
+            wins ++;
+            document.getElementById("numOfWins").innerHTML = "Wins: " + wins;
+            document.getElementById("numOfLosses").innerHTML = "Losses: " + losses;
+            wordIndex ++;
+            remainingGuesses = 8;
+            document.getElementById("guessesLeft").innerHTML = "Guesses remaining: " +remainingGuesses;
+            notInWord = [];
+            document.getElementById("lettersGuessed").innerHTML = "Letters already guessed: " + notInWord;
+            changeWord(words[wordIndex]); 
           
-    }
-    else if (wordIndex >= words.length) {
-        document.querySelector("body").innerHTML = "<p id='winScreen'>Congratulations! You have correctly guessed all of the characters!</p>";
+        }
+        else if (wordIndex >= words.length) {
+            document.querySelector("body").innerHTML = "<p id='winScreen'>Congratulations! You have correctly guessed all of the characters!</p>";
+        }
     }
     
-}
+    else if (restartGame){
+        losses ++;
+        document.getElementById("numOfLosses").innerHTML = "Losses: " + losses;
+        restartRound(words[wordIndex]);  
+    }
+}   
 
 
 
